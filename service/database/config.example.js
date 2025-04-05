33b1c91135bf8cf68c2b7099c2c97a9fdcc05aa0
teun <teunstrik@yahoo.nl>
2025-04-06 01:27:54 +0200

    Add example configuration file to service

---
+++ service/database/config.example.js
@@ -0,0 +1,23 @@
+module.exports = {
+    mysql: {
+        database: 'database',
+        username: 'username',
+        password: 'password',
+        dialect: 'mysql',
+        dialectOptions: {
+            charset: 'utf8mb4',
+        },
+    },
+
+    mysql_testing: {
+        database: 'database',
+        username: 'username',
+        password: 'password',
+        dialect: 'mysql',
+    },
+
+    site: 'http://localhost:8080',
+
+    hostname: '0.0.0.0',
+    port: 3000,
+};
